e7334b727d39383dbcf493583e764fea95dd076a
Yuri Galvão <90591430+gvao@users.noreply.github.com>
2022-08-31 15:57:55 -0300

    responsividade de varios arquivos
    mudei para funcionar com varios arquivos

--- index.js
+++ index.js
@@ -13,81 +13,53 @@ function readJson(file, callback) {
 }
 
 $originalFile.addEventListener("input", (event) => {
-  const file = event.target.files[0];
+  const files = event.target.files;
 
-  console.log(file.name);
+  [...files].forEach((file) => {
 
-  $downloas_csv.setAttribute("download", file.name.replace(`.json`, ".csv"));
+    $downloas_csv.setAttribute("download", file.name.replace(`.json`, ".csv"));
 
-  readJson(file, (json) => {
-    // console.log(json);
+    readJson(file, (json) => {
+      // console.log(json);
 
-    const extracaoInfosImportantes = json.reduce(
-      (
-        acc,
-        {
-          detalhesCliente: {
+      const extracaoInfosImportantes = json.reduce(
+        (
+          acc,
+          {
+            detalhesCliente: {
+              brisa_movel,
+              documento,
+              data_ativacao,
+              data_criacao,
+              data_nascimento,
+              identidade,
+              nome: nome_cliente,
+              telefone,
+              telefone2,
+              telefone3,
+            },
+            enderecos,
+          }
+        ) => {
+          let data = {
             brisa_movel,
             documento,
             data_ativacao,
             data_criacao,
             data_nascimento,
             identidade,
-            nome: nome_cliente,
+            nome_cliente,
             telefone,
             telefone2,
             telefone3,
-          },
-          enderecos,
-        }
-      ) => {
-        let data = {
-          brisa_movel,
-          documento,
-          data_ativacao,
-          data_criacao,
-          data_nascimento,
-          identidade,
-          nome_cliente,
-          telefone,
-          telefone2,
-          telefone3,
-        };
+          };
 
-        enderecos.map(
-          ({
-            bairro,
-            cep,
-            cidade_nome,
-            id: idEndereco,
-            latitude,
-            longitude,
-            logradouro,
-            numero,
-            ponto_referencia1,
-            ponto_referencia2,
-            uf,
-
-            cliente: {
-              ativo: cliente_ativo,
-              brisa_movel,
-              classificacao,
-              classificacao_valor,
-              data_criacao,
-              data_nascimento,
-              email,
-              email2,
-              grande_empresa,
-            },
-
-            contratos,
-          }) => {
-            data = {
-              ...data,
+          enderecos.map(
+            ({
               bairro,
               cep,
               cidade_nome,
-              idEndereco,
+              id: idEndereco,
               latitude,
               longitude,
               logradouro,
@@ -95,90 +67,117 @@ $originalFile.addEventListener("input", (event) => {
               ponto_referencia1,
               ponto_referencia2,
               uf,
-              cliente_ativo,
-              brisa_movel,
-              classificacao,
-              classificacao_valor,
-              data_criacao,
-              data_nascimento,
-              email,
-              email2,
-              grande_empresa,
-            };
 
-            contratos.map(
-              ({
-                ativo: contrato_ativo,
-                data_cancelado,
-                data_contratacao,
-                data_primeira_ativacao,
-                id: idContrato,
-                pessoa_juridica,
-                plano_principal: {
-                  categoria_tipo,
-                  nome: plano,
-                  radio,
-                  valor,
-                  valor_corporativo,
-                },
-              }) => {
-                data = {
-                  ...data,
-                  contrato_ativo,
+              cliente: {
+                ativo: cliente_ativo,
+                brisa_movel,
+                classificacao,
+                classificacao_valor,
+                data_criacao,
+                data_nascimento,
+                email,
+                email2,
+                grande_empresa,
+              },
+
+              contratos,
+            }) => {
+              data = {
+                ...data,
+                bairro,
+                cep,
+                cidade_nome,
+                idEndereco,
+                latitude,
+                longitude,
+                logradouro,
+                numero,
+                ponto_referencia1,
+                ponto_referencia2,
+                uf,
+                cliente_ativo,
+                brisa_movel,
+                classificacao,
+                classificacao_valor,
+                data_criacao,
+                data_nascimento,
+                email,
+                email2,
+                grande_empresa,
+              };
+
+              contratos.map(
+                ({
+                  ativo: contrato_ativo,
                   data_cancelado,
                   data_contratacao,
                   data_primeira_ativacao,
-                  idContrato,
+                  id: idContrato,
                   pessoa_juridica,
-                  categoria_tipo,
-                  plano,
-                  radio,
-                  valor,
-                  valor_corporativo,
-                };
+                  plano_principal: {
+                    categoria_tipo,
+                    nome: plano,
+                    radio,
+                    valor,
+                    valor_corporativo,
+                  },
+                }) => {
+                  data = {
+                    ...data,
+                    contrato_ativo,
+                    data_cancelado,
+                    data_contratacao,
+                    data_primeira_ativacao,
+                    idContrato,
+                    pessoa_juridica,
+                    categoria_tipo,
+                    plano,
+                    radio,
+                    valor,
+                    valor_corporativo,
+                  };
 
-                let newData = {};
+                  let newData = {};
 
-                for (key in data) {
-                  const val = data[key];
-                  newData = { ...newData, [key]: val || "" };
+                  for (key in data) {
+                    const val = data[key];
+                    newData = { ...newData, [key]: val || "" };
+                  }
+
+                  acc = [...acc, newData];
                 }
+              );
+            }
+          );
 
-                console.log(newData)
+          return acc;
+        },
+        []
+      );
 
-                acc = [...acc, newData];
-              }
-            );
-          }
-        );
+      // console.log(extracaoInfosImportantes);
 
-        return acc;
-      },
-      []
-    );
+      const header = Object.keys(extracaoInfosImportantes[0]);
 
-    // console.log(extracaoInfosImportantes);
+      let arr = [header];
 
-    const header = Object.keys(extracaoInfosImportantes[0]);
+      extracaoInfosImportantes.forEach((contrato) => {
+        const values = Object.values(contrato);
+        //   const header = arr[0];
 
-    let arr = [header];
+        arr.push(values);
+      });
 
-    extracaoInfosImportantes.forEach((contrato) => {
-      const values = Object.values(contrato);
-    //   const header = arr[0];
+      const dataFinal = arr.map((linha) => linha.join()).join(`\n`);
 
-      arr.push(values);
+      $downloas_csv.setAttribute(
+        `href`,
+        `data:text/csv;encoding=utf8,${encodeURIComponent(dataFinal)}`
+      );
+
+      $downloas_csv.click();
+
+      console.log(dataFinal);
     });
-
-    const dataFinal = arr.map((linha) => linha.join()).join(`\n`);
-
-    $downloas_csv.setAttribute(
-      `href`,
-      `data:text/csv;encoding=utf8,${encodeURIComponent(dataFinal)}`
-    );
-
-    $downloas_csv.click();
-
-    console.log(dataFinal);
   });
 });
